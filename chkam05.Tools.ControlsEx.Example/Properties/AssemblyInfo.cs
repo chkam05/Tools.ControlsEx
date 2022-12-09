494d9a62d9d40212928218954ab79423510ed63b
chkam05 <kamil.k3@outlook.com>
2022-12-09 16:26:09 +0100

    Updated version in example

--- chkam05.Tools.ControlsEx.Example/Properties/AssemblyInfo.cs
+++ chkam05.Tools.ControlsEx.Example/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Windows;
 // przy użyciu symbolu „*”, tak jak pokazano poniżej:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.2.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.2.0")]
